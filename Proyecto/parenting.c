295b3cc7ce68c60520f048f9bd43c32f94090d56
Estefy <A00344189@itesm.mx>
2018-03-31 20:52:38 -0600

    Update parenting.c

--- Proyecto/parenting.c
+++ Proyecto/parenting.c
@@ -5,7 +5,7 @@
 int main()
 {
     int t, ac, aj;
-    int c[101], d[101], j[101], k[101];
+    int c[5], d[5], j[5], k[5];
     scanf("%d", &t);
 
     for (int i = 0; i < t; i++) {
@@ -13,18 +13,17 @@ int main()
 
         for (int m = 0; m < ac; m++)
             scanf("%d %d", &c[m], &d[m]);
-        for (int m= 0; m < aj; m++)
+        for (int m = 0; m < aj; m++)
             scanf("%d %d", &j[m], &k[m]);
 
         int result = 2;
 
         if (ac == 2 &&
-            (d[1] - c[0] > 720 && c[1] - d[0] < 720 ||
-             d[0] - c[1] > 720 && c[0] - d[1] < 720))
+            ( (d[1] > 720 && d[0] <= 720) ||  (d[1] <= 720 && d[0] > 720) ))
             result = 4;
         if (aj == 2 &&
-            (k[1] - j[0] > 720 && j[1] - k[0] < 720 ||
-              k[0] - j[1] > 720 && j[0] - k[1] < 720))
+         ( (k[1] > 720 && k[0] <= 720) ||  (k[1] <= 720 && k[0] > 720) ))
+            
             result = 4;
 
         printf("Case #%d: %d\n", i + 1, result);
