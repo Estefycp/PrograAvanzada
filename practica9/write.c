f70c2db38d1d1e86d416d2eb883bfb689a905a66
Estefy <estefy.cp@hotmail.com>
2018-04-09 19:52:43 -0500

    practica 9

---
+++ practica9/write.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct
+{
+    char firstName[20];
+    char lastName[20];
+    short id;
+    char semester[3];
+} Student;
+
+int main()
+{
+    char *dbName = "student.dat";
+    Student pAvanzada[10];
+    for (int i = 0; i < 10; ++i)
+    {
+        sprintf(pAvanzada[i].firstName, "name%i", i);
+        sprintf(pAvanzada[i].lastName, "lasname%i", i);
+        pAvanzada[i].id = i;
+        sprintf(pAvanzada[i].semester, "S%i", i);
+    }
+    FILE *data = fopen(dbName, "wb");
+    fwrite(pAvanzada, sizeof(Student), 10, data);
+    fclose(data);
+    return 0;
+}
